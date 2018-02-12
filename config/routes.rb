3fb671ab6951117e205cbadad15a321b5a677c45
João Ricardo Bastos <joaoricardobastos@gmail.com>
2018-02-12 12:31:25 -0300

    initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :v1 do
+
+    namespace :client do
+      resources :sessions, only: [:create, :destroy]
+    end
+  end
 end
